9a49989404bc8f660fa568df021fc4e17325c63c
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2024-09-20 20:22:48 +0530

    Time: 2 ms (62.79%), Space: 45.7 MB (81.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,52 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int i = 0, j = 0, m1 = 0, m2 = 0;
-
-        for (int count = 0; count <= (n + m) / 2; count++) {
-            m2 = m1;
-            if (i != n && j != m) {
-                if (nums1[i] > nums2[j]) {
-                    m1 = nums2[j++];
-                } else {
-                    m1 = nums1[i++];
-                }
-            } else if (i < n) {
-                m1 = nums1[i++];
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        
+        int ind1 = (n - 1) / 2;  // First middle index (for even, it's the smaller of two middle elements)
+        int ind2 = n / 2;        // Second middle index (for even, it's the larger one)
+        int cnt = 0;             // To track the number of elements processed
+        int ind1el = -1, ind2el = -1; // To store the two median elements
+        
+        int i = 0, j = 0;  // Pointers for nums1 and nums2
+        
+        while (i < n1 && j < n2) {
+            int val;
+            if (nums1[i] < nums2[j]) {
+                val = nums1[i];
+                i++;
             } else {
-                m1 = nums2[j++];
+                val = nums2[j];
+                j++;
             }
+            
+            if (cnt == ind1) ind1el = val; 
+            if (cnt == ind2) ind2el = val; 
+            cnt++;
         }
 
-        if ((n + m) % 2 == 1) {
-            return (double) m1;
-        } else {
-            double ans = (double) m1 + (double) m2;
-            return ans / 2.0;
+
+        while (i < n1) {
+            if (cnt == ind1) ind1el = nums1[i];
+            if (cnt == ind2) ind2el = nums1[i];
+            i++;
+            cnt++;
         }
+
+        while (j < n2) {
+            if (cnt == ind1) ind1el = nums2[j];
+            if (cnt == ind2) ind2el = nums2[j];
+            j++;
+            cnt++;
+        }
+
+    
+        if (n % 2 == 1) {
+            return (double) ind2el;
+        }
+        
+
+        return (double) (ind1el + ind2el) / 2.0;
     }
 }
