40bd8d43c55011750702d1f87156a0ed5b6d5bbb
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2024-09-29 03:57:52 +0530

    Time: 19 ms (51.04%), Space: 44.6 MB (29.29%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,16 +1,14 @@
 class Solution {
 
-    // Function to calculate the sum of subarray minimums
     public static long subArrayMin(int[] nums) {
         int length = nums.length;
         int[] left = new int[length];
         int[] right = new int[length];
-        
+
         Arrays.fill(left, -1);
         Arrays.fill(right, length);
         Deque<Integer> stack = new ArrayDeque<>();
 
-        // Finding previous smaller element for each index
         for (int i = 0; i < length; ++i) {
             while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
                 stack.pop();
@@ -20,10 +18,9 @@ class Solution {
             }
             stack.push(i);
         }
-        
+
         stack.clear();
 
-        // Finding next smaller element for each index
         for (int i = length - 1; i >= 0; --i) {
             while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
                 stack.pop();
@@ -34,28 +31,25 @@ class Solution {
             stack.push(i);
         }
 
-        // Calculating the sum of subarray minimums
         long sumOfSubarrayMinimums = 0;
         for (int i = 0; i < length; ++i) {
-            long countLeft = i - left[i];  // Number of subarrays starting from the left
-            long countRight = right[i] - i;  // Number of subarrays ending on the right
+            long countLeft = i - left[i];
+            long countRight = right[i] - i;
             sumOfSubarrayMinimums += countLeft * countRight * nums[i];
         }
 
         return sumOfSubarrayMinimums;
     }
 
-    // Function to calculate the sum of subarray maximums
     public static long subArrayMax(int[] nums) {
         int length = nums.length;
         int[] left = new int[length];
         int[] right = new int[length];
-        
+
         Arrays.fill(left, -1);
         Arrays.fill(right, length);
         Deque<Integer> stack = new ArrayDeque<>();
 
-        // Finding previous greater element for each index
         for (int i = 0; i < length; ++i) {
             while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
                 stack.pop();
@@ -68,7 +62,6 @@ class Solution {
 
         stack.clear();
 
-        // Finding next greater element for each index
         for (int i = length - 1; i >= 0; --i) {
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
                 stack.pop();
@@ -79,21 +72,19 @@ class Solution {
             stack.push(i);
         }
 
-        // Calculating the sum of subarray maximums
         long sumOfSubarrayMaximums = 0;
         for (int i = 0; i < length; ++i) {
-            long countLeft = i - left[i];  // Number of subarrays starting from the left
-            long countRight = right[i] - i;  // Number of subarrays ending on the right
+            long countLeft = i - left[i];
+            long countRight = right[i] - i;
             sumOfSubarrayMaximums += countLeft * countRight * nums[i];
         }
 
         return sumOfSubarrayMaximums;
     }
 
-    // Function to calculate the sum of subarray ranges
     public long subArrayRanges(int[] nums) {
-        long max = subArrayMax(nums);  // Sum of subarray maximums
-        long min = subArrayMin(nums);  // Sum of subarray minimums
-        return max - min;  // Difference between max and min sums
+        long max = subArrayMax(nums);
+        long min = subArrayMin(nums);
+        return max - min;
     }
 }
