1280b8998362036b2c1619692c4284b56c2076d6
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2024-09-29 03:53:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,31 +1,46 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        long sum = 0;
+    public static long subArrayMin(int[] nums){
+        int length = nums.length;
+        int[] left = new int[length];
+        int[] right = new int[length];
+        
+        Arrays.fill(left, -1);
+        Arrays.fill(right, length);
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < length; ++i) {
+            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                left[i] = stack.peek();
+            }
+            stack.push(i);
+        } 
+        stack.clear();
+        for (int i = length - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                right[i] = stack.peek();
+            }
+            stack.push(i);
+        }  
+        int mod = (int) 1e9 + 7;
+        long answer = 0;  
+        for (int i = 0; i < length; ++i) {
+            answer += (long) (i - left[i]) * (right[i] - i) % mod * nums[i] % mod;
+            answer %= mod;
+        }
+      
+        return answer;
+    }
+    public static long subArrayMax(int[] nums){
         int N = nums.length;
-        int[] pse = new int[N];
-        int[] nse = new int[N];
-        Stack<Integer> st = new Stack<>();
-        for(int i=N-1;i>=0;i--){
-            while(!st.isEmpty() && nums[st.peek()]>=nums[i])
-                st.pop();
-            nse[i] = st.isEmpty()?N:st.peek();
-            st.push(i);
-        }
-        st = new Stack<>();
-        for(int i=0;i<N;i++){
-            while(!st.isEmpty() && nums[st.peek()]>nums[i])
-                st.pop();
-            pse[i] = st.isEmpty()?-1:st.peek();
-            st.push(i);
-        }
-        long sumOfSubarrayMinimums = 0;
-        for(int i=0;i<N;i++){
-            long cnteleleft = i-pse[i];
-            long cnteleright = nse[i]-i;
-            sumOfSubarrayMinimums+=cnteleleft*cnteleright*nums[i];
-        }
         int[] pge = new int[N];
         int[] nge = new int[N];
+        Stack<Integer> st = new Stack<>();
         st = new Stack<>();
         for(int i=N-1;i>=0;i--){
             while(!st.isEmpty() && nums[st.peek()]<=nums[i])
@@ -46,6 +61,11 @@ class Solution {
             long cnteleright = nge[i]-i;
             sumOfSubarrayMaximums+=cnteleleft*cnteleright*nums[i];
         }
-        return sumOfSubarrayMaximums-sumOfSubarrayMinimums;
+        return sumOfSubarrayMaximums;
+    }
+    public long subArrayRanges(int[] nums) {
+        long max = subArrayMax(nums);
+        long min = subArrayMin(nums);
+        return max-min;
     }
 }
