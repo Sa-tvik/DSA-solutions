6e864a90f760deffc50a38ac8be97b7f79b997cb
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2024-09-29 03:58:04 +0530

    Time: 18 ms (56.33%), Space: 44.5 MB (29.29%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,90 +1,38 @@
 class Solution {
 
-    public static long subArrayMin(int[] nums) {
-        int length = nums.length;
-        int[] left = new int[length];
-        int[] right = new int[length];
-
-        Arrays.fill(left, -1);
-        Arrays.fill(right, length);
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        for (int i = 0; i < length; ++i) {
-            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-
-        stack.clear();
-
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-
-        long sumOfSubarrayMinimums = 0;
-        for (int i = 0; i < length; ++i) {
-            long countLeft = i - left[i];
-            long countRight = right[i] - i;
-            sumOfSubarrayMinimums += countLeft * countRight * nums[i];
-        }
-
-        return sumOfSubarrayMinimums;
-    }
-
-    public static long subArrayMax(int[] nums) {
-        int length = nums.length;
-        int[] left = new int[length];
-        int[] right = new int[length];
-
-        Arrays.fill(left, -1);
-        Arrays.fill(right, length);
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        for (int i = 0; i < length; ++i) {
-            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-
-        stack.clear();
-
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-
-        long sumOfSubarrayMaximums = 0;
-        for (int i = 0; i < length; ++i) {
-            long countLeft = i - left[i];
-            long countRight = right[i] - i;
-            sumOfSubarrayMaximums += countLeft * countRight * nums[i];
-        }
-
-        return sumOfSubarrayMaximums;
-    }
-
     public long subArrayRanges(int[] nums) {
-        long max = subArrayMax(nums);
-        long min = subArrayMin(nums);
-        return max - min;
+        int n = nums.length;
+        long answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        // Find the sum of all the minimum.
+        for (int right = 0; right <= n; ++right) {
+            while (
+                !stack.isEmpty() &&
+                (right == n || nums[stack.peek()] >= nums[right])
+            ) {
+                int mid = stack.peek();
+                stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                answer -= (long) nums[mid] * (right - mid) * (mid - left);
+            }
+            stack.add(right);
+        }
+
+        // Find the sum of all the maximum.
+        stack.clear();
+        for (int right = 0; right <= n; ++right) {
+            while (
+                !stack.isEmpty() &&
+                (right == n || nums[stack.peek()] <= nums[right])
+            ) {
+                int mid = stack.peek();
+                stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                answer += (long) nums[mid] * (right - mid) * (mid - left);
+            }
+            stack.add(right);
+        }
+        return answer;
     }
 }
