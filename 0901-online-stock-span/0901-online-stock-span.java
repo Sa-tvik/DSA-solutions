06de3c5731cef66d05cf1fcba071ad5655f970c9
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2024-10-02 01:04:20 +0530

    Time: 30 ms (70.3%), Space: 55.8 MB (7.13%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -6,7 +6,7 @@ class StockSpanner {
     
     public int next(int price) {
         int span  = 1;
-        while(!st.isEmpty() && st.peek()[0]<price){
+        while(!st.isEmpty() && st.peek()[0]<=price){
             span += st.pop()[1];
         }
         st.push(new int[] {price,span});
