d2a6f1e734fbe382c3b76ec7e800c52ecb5119ed
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2024-12-25 20:47:13 +0530

    Time: 2 ms (95.65%), Space: 45.8 MB (6.66%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -13,32 +13,38 @@
  *     }
  * }
  */
-class Solution {
-    private void inOrder(TreeNode node, List<Integer> res) {
-        if (node == null) {
-            return;
+ class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+    private boolean reverse;
+    BSTIterator(TreeNode root, boolean reverse) {
+        this.reverse = reverse;
+        push(root);
+    }
+    int next() {
+        TreeNode top = st.pop();
+        push(!reverse ? top.right : top.left);
+        return top.val;
+    }
+    private void push(TreeNode root) {
+        while (root != null) {
+            st.push(root);
+            root = !reverse ? root.left : root.right;
         }
+    }
+}
 
-        inOrder(node.left, res);
-        res.add(node.val);
-        inOrder(node.right, res);
-    }  
-    public boolean findTarget(TreeNode root, int k) {
-        List<Integer> res = new ArrayList<>();
-        // if(res.si)
-        inOrder(root, res);
-        int j = res.size()-1;
-        int i = 0;
-        while(i<j){
-            int sum = res.get(i) + res.get(j);
-            if(sum == k){
-                return true;
-            }
-            if(sum<k){
-                i++;
-            }else if(sum>k){
-                j--;
-            }
+class Solution {
+     public boolean findTarget(TreeNode root, int k) {
+        BSTIterator leftItr = new BSTIterator(root, false);
+        BSTIterator rightItr = new BSTIterator(root, true);
+
+        int left = leftItr.next(), right = rightItr.next();
+        while (left < right) {
+            if (left + right == k) return true;
+            if (left + right < k)
+                left = leftItr.next();
+            else
+                right = rightItr.next();
         }
         return false;
     }
