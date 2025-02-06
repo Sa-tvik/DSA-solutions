b0bf44bcbc983ad4fd27beb5955ada8d251fff10
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2025-02-07 02:56:44 +0530

    Time: 6 ms (99.2%), Space: 45.6 MB (15.53%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,32 @@
+class Solution {
+    static boolean helper(int[] piles, int h, int k){
+        int hoursNeeded = 0;
+        for (int pile : piles) {
+            hoursNeeded += (pile + k - 1) / k; 
+            if (hoursNeeded > h) {
+                return false; 
+            }
+        }
+        return true;      
+    }
+    public int minEatingSpeed(int[] piles, int h) { 
+        int max = 0;
+        for(int i = 0;i<piles.length;i++){
+            max = Math.max(max,piles[i]);
+        }
+        int low = 1;
+        int high = max;
+        int min = max;
+
+        while(low<=high){
+            int mid =   low + (high-low)/2;
+            if(!helper(piles, h, mid)){
+                low = mid+1;
+            }else {
+                min = mid;
+                high = mid-1;
+            }
+        }
+        return min;
+    }
+}
