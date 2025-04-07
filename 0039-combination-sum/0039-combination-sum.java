1155e7adf2ddc072d54f42ed29e709f651ce4949
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2025-04-07 17:18:47 +0530

    Time: 1 ms (99.9%), Space: 44.7 MB (46.62%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public  void helper(int ind,int[] candidates, int target,List<List<Integer>> ans, List<Integer> arr){
+        if(ind == candidates.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(arr));
+            }
+            return;
+        }
+        if(candidates[ind]<=target){
+            arr.add(candidates[ind]);
+            helper(ind, candidates, target - candidates[ind], ans, arr);
+            arr.remove(arr.size()-1);
+        }
+        helper(ind+1, candidates, target, ans, arr);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+}
