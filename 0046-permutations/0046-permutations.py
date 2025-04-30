3df9d7861e716adf138a7b3614b0cd15f0b2379f
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2025-05-01 05:04:12 +0530

    Time: 3 ms (52.55%), Space: 12.3 MB (99.95%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,19 @@
 class Solution(object):
-
-
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def helper(ind, nums, ans, arr):
-            if len(nums) == len(arr):
+        def helper(arr, ans):
+            if len(arr) == len(nums):
                 ans.append(list(arr))
-            else:
-                for i in range(len(nums)):
-                    if nums[i] not in arr:
-                        arr.append(nums[i])
-                        helper(i+1, nums, ans, arr)
-                        arr.pop()
+                return
+            for i in range(len(nums)):
+                if nums[i] not in arr:
+                    arr.append(nums[i])
+                    helper(arr, ans)
+                    arr.pop()
 
         ans = []
-        helper(0,nums,ans,[])
+        helper([], ans)
         return ans
-        
