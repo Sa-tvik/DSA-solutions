1cd9a2f624521195575faf2966a4930c3a9eb344
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2025-05-22 15:12:28 +0530

    Time: 41 ms (98.83%), Space: 19.6 MB (71.37%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -5,12 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        heap = []
-        for i in range(len(nums)):
-            if len(heap)>=k:
-                heapq.heappushpop(heap,nums[i])
-            else:
-                heapq.heappush(heap,nums[i])
+        min_value = min(nums)
+        max_value = max(nums)
 
-        return heap[0]
+        count = [0] * (max_value - min_value + 1)
+
+        for num in nums:
+            count[num - min_value] += 1
+        
+        remaining = k
+        for i in range(len(count) - 1, -1,-1):
+            remaining -= count[i] 
+
+            if remaining <= 0:
+                return i + min_value
         
