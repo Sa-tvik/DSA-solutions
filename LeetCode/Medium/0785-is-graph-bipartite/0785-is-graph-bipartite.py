ddd60a1057d5d4a3ad3551353f52aba89e01574d
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2025-06-07 18:55:10 +0530

    Time: 15 ms (7.61%), Space: 19.2 MB (9.38%) - LeetHub

---
+++ LeetCode/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+
+        q = deque()
+        vis = defaultdict(list)
+        for start in range(len(graph)):
+            if start in vis:
+                continue
+            q.append((start, 0))
+            while len(q)>0:
+                x,y = q.popleft()
+                if x in vis and y not in vis[x]: 
+                    return False
+                elif x in vis and y in vis[x]:
+                    continue
+                vis[x].append(y)
+                for i in range(len(graph[x])):
+                    q.append([graph[x][i],y^1])
+        return True
