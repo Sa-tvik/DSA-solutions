a466e740a2b31f6a653b898dcebfaac36417de02
Sa-tvik <134994552+Sa-tvik@users.noreply.github.com>
2025-06-14 04:03:45 +0530

    Time: 1828 ms (8.87%), Space: 17.2 MB (15.55%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.py
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.py
@@ -1,29 +1,25 @@
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
         n = len(coins)
-
-        prev = [0] * (amount + 1)  
-        cur = [0] * (amount + 1)  
-
-
-        for i in range(0, 1 + amount):
-            if i % coins[0] == 0:
-                prev[i] = i // coins[0]
+        dp = [[-1 for j in range(amount+1)] for i in range(n)]
+        for i in range(amount+1):
+            if i%coins[0] == 0:
+                dp[0][i] = i//coins[0]
             else:
-                prev[i] = int(1e9)
+                dp[0][i] = float('inf')
+        
+        for idx in range(1,n):
+            for target in range(amount+1):          
+                notTaken = dp[idx-1][target]
+                taken = float('inf')
+                if coins[idx]<=target:
+                    taken = 1 + dp[idx][target-coins[idx]]
+                dp[idx][target] = min(taken, notTaken)
 
-        for ind in range(1, n):
-            for target in range(amount + 1):
-
-                not_take = prev[target]
-                take = int(1e9)          
-
-                if coins[ind] <= target:
-                    take = 1 + cur[target - coins[ind]]   
-                cur[target] = min(not_take, take) 
-            prev = cur 
-        ans = prev[amount]
-
-        if ans >= int(1e9):
-            return -1
-        return ans
+        num = dp[n-1][amount]
+        return num if num != float('inf') else -1
