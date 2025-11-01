692c224f70a2852603e1abe953506b86f3ffc11e
Satvik Agarwal <134994552+Sa-tvik@users.noreply.github.com>
2025-11-02 04:59:31 +0530

    Time: 451 ms (77.62%), Space: 43.7 MB (26.9%) - LeetHub

--- LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,16 +1,27 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n = len(text1)
-        m = len(text2)
-        dp = [0]*(m+1)
-        
-         
-        for ind1 in range(1,n+1):
-            temp = [0]*(m+1)
-            for ind2 in range(1,m+1):
-                if text1[ind1-1] == text2[ind2-1]:
-                    dp[ind2] = 1 + dp[ind2-1]
-                else:
-                    dp[ind2] = max(dp[ind2-1], dp[ind2])
+        m,n = len(text1), len(text2)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
 
-        return dp[m]
+        for i in range(m): 
+            if text1[i] == text2[0]: 
+                dp[i][0] = 1 
+            else: 
+                dp[i][0] = dp[i-1][0]
+
+        for i in range(1,n):
+            if text1[0] == text2[i]:
+                dp[0][i] = 1
+            else:
+                dp[0][i] = dp[0][i-1]
+
+        for ind1 in range(1, m):
+            for ind2 in range(1,n):
+                if text1[ind1] == text2[ind2]:
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1])
+
+        return dp[m-1][n-1]
+        
+            
