31ada2d85cc9989ba6ca3a00d0f5032c2c598850
Satvik Agarwal <134994552+Sa-tvik@users.noreply.github.com>
2025-11-11 03:13:20 +0530

    Time: 43 ms (20.73%), Space: 20.5 MB (38.72%) - LeetHub

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,28 +1,12 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        dic = {}
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dic = defaultdict(int)
+        dic[0] = 1
         pSum = 0
         res = 0
-        dic[0] = 1
         for i in range(len(nums)):
             pSum += nums[i]
             if pSum-k in dic:
-                temp = dic[pSum-k]
-                res += temp
-            if pSum in dic:
-                temp = dic[pSum]
-                dic[pSum] = temp+1
-            else:
-                dic[pSum] = 1
-
+                res += dic[pSum-k]
+            dic[pSum]+=1
         return res
-
-
-            
-
-        
