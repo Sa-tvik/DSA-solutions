ed72f7b431bf109b5dfb8e523138002c55cafdfb
Satvik Agarwal <134994552+Sa-tvik@users.noreply.github.com>
2025-11-11 01:27:43 +0530

    Time: 0 ms (100%), Space: 18.1 MB (26.61%) - LeetHub

--- LeetCode/Medium/0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ LeetCode/Medium/0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -10,13 +10,13 @@ class Solution:
         Do not return anything, modify root in-place instead.
         """
         current = root
-
         while current:
             if current.left:
-                predecessor = current.left
-                while predecessor.right:
-                    predecessor = predecessor.right
-                predecessor.right = current.right
+                temp = current.left
+                while temp.right:
+                    temp = temp.right
+                temp.right = current.right
                 current.right = current.left
                 current.left = None
             current = current.right
+        
